f5fa2e3505bb2d1ff129f9d27a60ac460953febc
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2023-06-24 11:24:08 -0300

    git init

---
+++ src/components/Room.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card width="400" title="This is a title" subtitle="This is a subtitle" text="This is content">
+        <v-btn icon="mdi-google-classroom" size="x-large"></v-btn>
+    </v-card>
+</template>
+
+<script setup>
+</script>
+
+<script>
+</script>
+
+<style scoped></style>
